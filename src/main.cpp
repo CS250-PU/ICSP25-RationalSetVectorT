f76fdffd969b4eb38ec0b1ca194b2d698ff5edce
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-03-07 19:06:18 +0000

    Updated RationalSet template

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,36 @@
 // Hours:       
 //******************************************************************************
 
+/* TODO #1: Ask the user to enter a Rational number then output the Rational
+            number. Interaction is to look like:
+            Enter a Rational: 1/2
+            Rational entered: 1/2
+
+   TODO #2: Ask the user to enter a RationalSet number then output the 
+            RationalSet. Interaction is to look like:
+            Enter a RationalSet: {1/2 2/3 3/4}
+            RationalSet entered: {1/2, 2/3, 3/4}
+
+   TODO #3: Create a vector that can hold a unknown number of RationalSets.
+            Read in each RationalSet from the file RationalSets.txt into
+            the vector. Then output each RationalSet. Interaction is to look
+            like:
+            RationalSets:
+            {}
+            {1/2}
+            {1/2, 2/3}
+            {2/3, 3/4, 4/5}
+
+   TODO #4: Unit test operator<<
+
+   TODO #5: Unit test operator>>
+*/
+
 #include <iostream>
 #include "../include/SampleClass.h"
 
 int main () {
-  SampleClass cSampleClass;
 
-  std::cout << "Factorial of 1: " << cSampleClass.factorial (0) << std::endl;
 
   std::cout << "Process Completed" << std::endl;
 
